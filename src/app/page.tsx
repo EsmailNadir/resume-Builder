6d138701e87ee6d94c98f2869af38f6862d4ade0
EsmailNadir <112122818+EsmailNadir@users.noreply.github.com>
2024-11-25 17:37:58 -0800

    created forms for resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import FormInput from "./components/WorkExperienceForm";
+'use client'
+
+import WorkExperienceForm from "./components/WorkExperienceForm"
+import PersonalForm from "./components/PersonalForm"
+import React from "react"
+import SummaryForm from "./components/summaryForm"
 
 export default function Home() {
+  const handleFormSubmit = (data: any) =>{
+    console.log("FormSubmitted:", data);
+    
+  }
   return (
-    <FormInput/>
+    <>
+    <WorkExperienceForm onSubmit={handleFormSubmit}/>
+    <PersonalForm  onSubmit={handleFormSubmit}/>
+    <SummaryForm onSubmit={handleFormSubmit}/>
+    </>
   )
+    
+  
 }
