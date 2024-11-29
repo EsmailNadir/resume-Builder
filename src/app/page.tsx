7ed79839331b7db2d63459bc8b7aa4c4f713a686
EsmailNadir <112122818+EsmailNadir@users.noreply.github.com>
2024-11-29 14:51:21 -0800

    Save changes in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,42 @@
 import WorkExperienceForm from "./components/WorkExperienceForm"
 import PersonalForm from "./components/PersonalForm"
 import React from "react"
+import { useState } from "react"
 import SummaryForm from "./components/summaryForm"
 import SkillsForm from "./components/SkillsForm"
 import EducationForm from "./components/EducationForm"
 
+
+
 export default function Home() {
+
+  const [showForm, setShowForm] = useState<string | null>(null)
+
   const handleFormSubmit = (data: any) =>{
     console.log("FormSubmitted:", data);
     
   }
   return (
-    <>
-    <WorkExperienceForm onSubmit={handleFormSubmit}/>
-    <PersonalForm  onSubmit={handleFormSubmit}/>
-    <SummaryForm onSubmit={handleFormSubmit}/>
-    <SkillsForm onSubmit={handleFormSubmit}/>
-    <EducationForm onSubmit={handleFormSubmit}/>
+    <div>
+      <button className=" my-6 mx-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=> setShowForm('WorkExperienceForm')}> Work Experience</button>
+      <button  className=" my-6 mx-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=> setShowForm('PersonalForm')}> Personal Form</button>
+      <button className="my-6 mx-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=> setShowForm('SummaryForm')}> Summary Form</button>
+
+      <div>
+        {showForm === 'WorkExperienceForm' && (
+          <WorkExperienceForm onSubmit={handleFormSubmit}/>
+        )}
+        {showForm === 'PersonalForm' && (
+          <PersonalForm  onSubmit={handleFormSubmit}/>
+        )}
+        {showForm === 'SummaryForm' && (
+          <SummaryForm onSubmit={handleFormSubmit}/>
+        )}
+      </div>
+    </div>
+
     
-    </>
+    
   )
     
   
